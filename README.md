d83a5f1bb89216853e42502ea19cebce3a76e53c
Johanni Brea <jbrea@users.noreply.github.com>
2018-08-10 11:51:59 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # RLEnvGym
 
-[![Build Status](https://travis-ci.com/Johanni Brea/RLEnvGym.jl.svg?branch=master)](https://travis-ci.com/Johanni Brea/RLEnvGym.jl)
+[![Build Status](https://travis-ci.com/JuliaReinforcementLearning/RLEnvGym.jl.svg?branch=master)](https://travis-ci.com/JuliaReinforcementLearning/RLEnvGym.jl)
 
-[![Coverage Status](https://coveralls.io/repos/Johanni Brea/RLEnvGym.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/Johanni Brea/RLEnvGym.jl?branch=master)
+[![Coverage Status](https://coveralls.io/repos/JuliaReinforcementLearning/RLEnvGym.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/JuliaReinforcementLearning/RLEnvGym.jl?branch=master)
 
-[![codecov.io](http://codecov.io/github/Johanni Brea/RLEnvGym.jl/coverage.svg?branch=master)](http://codecov.io/github/Johanni Brea/RLEnvGym.jl?branch=master)
+[![codecov.io](http://codecov.io/github/JuliaReinforcementLearning/RLEnvGym.jl/coverage.svg?branch=master)](http://codecov.io/github/JuliaReinforcementLearning/RLEnvGym.jl?branch=master)
+
+Making the [OpenAI gym](https://github.com/openai/gym) environments available to the [Julia Reinforcement Learning](https://github.com/jbrea/ReinforcementLearning.jl) package.
--- src/RLEnvGym.jl
+++ src/RLEnvGym.jl
@@ -1,7 +1,57 @@
 module RLEnvGym
 using Reexport
 @reexport using ReinforcementLearning
-import ReinforcementLearning:interact!, reset!, getstate
+import ReinforcementLearning:interact!, reset!, getstate, plotenv
+using PyCall
+@pyimport gym
+# @pyimport roboschool
 
+function getspace(space)
+    if pyisinstance(space, gym.spaces[:box][:Box])
+        ReinforcementLearning.Box(space[:low], space[:high])
+    elseif pyisinstance(space, gym.spaces[:discrete][:Discrete])
+        1:space[:n]
+    else
+        error("Don't know how to convert $(pytypeof(space)).")
+    end
+end
+mutable struct GymEnvState
+    done::Bool
+end
+struct GymEnv{TObject, TObsSpace, TActionSpace}
+    pyobj::TObject
+    observation_space::TObsSpace
+    action_space::TActionSpace
+    state::GymEnvState
+end
+function GymEnv(name::String)
+    pyenv = gym.make(name)
+    obsspace = getspace(pyenv[:observation_space])
+    actspace = getspace(pyenv[:action_space])
+    env = GymEnv(pyenv, obsspace, actspace, GymEnvState(false))
+    reset!(env)
+    env
+end
+
+function interactgym!(action, env)
+    if env.state.done 
+        s = reset!(env)
+        r = 0
+        d = false
+    else
+        s, r, d = env.pyobj[:step](action)
+    end
+    env.state.done = d
+    s, r, d
+end
+interact!(action, env::GymEnv) = interactgym!(action, env)
+interact!(action::Int64, env::GymEnv) = interactgym!(action - 1, env)
+reset!(env::GymEnv) = env.pyobj[:reset]()
+getstate(env::GymEnv) = (env.pyobj[:env][:state], false) # doesn't work for all envs
+
+plotenv(env::GymEnv, s, a, r, d) = env.pyobj[:render]()
+listallenvs() = gym.envs[:registry][:all]()
+
+export GymEnv, listallenvs
 
 end # module
